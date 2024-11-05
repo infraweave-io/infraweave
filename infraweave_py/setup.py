18ace4a0de698663f8179cf3c851c766861e6258
InfiniteTabs <InfiniteTabs@users.noreply.github.com>
2024-11-05 19:05:17 +0100

    fix: add python wheel build pipeline

---
+++ infraweave_py/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="infraweave_py",
+    version="0.0.1",
+    description="Your package description here",
+    author="Your Name",
+    author_email="your.email@example.com",
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.10',
+)
