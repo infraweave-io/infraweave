54447c1ebfb96ea894efcf5ae460fd8fd5345e12
InfiniteTabs <InfiniteTabs@users.noreply.github.com>
2024-12-08 13:50:58 +0100

    fix: remove false positive for dead_code

--- env_aws/src/utils.rs
+++ env_aws/src/utils.rs
@@ -1,5 +1,6 @@
 use aws_config::meta::region::RegionProviderChain;
 
+#[allow(dead_code)]
 pub async fn get_region() -> String {
     let region_provider = RegionProviderChain::default_provider().or_default_provider();
     let region = region_provider
