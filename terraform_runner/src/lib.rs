f4e8c3f02720fe67f66c3b6a412c43edd2bbdb59
InfiniteTabs <18383533+InfiniteTabs@users.noreply.github.com>
2025-11-05 21:32:48 +0100

    fix: add resource_changes to track sanitized changes in a structured format

--- terraform_runner/src/lib.rs
+++ terraform_runner/src/lib.rs
@@ -17,8 +17,8 @@ pub use opa::{
 pub use read::read_module_from_file;
 pub use runner::{run_terraform_runner, setup_misc};
 pub use terraform::{
-    run_terraform_command, set_up_provider_mirror, terraform_apply_destroy, terraform_init,
-    terraform_output, terraform_plan, terraform_show, terraform_show_after_apply,
+    record_apply_destroy_changes, run_terraform_command, set_up_provider_mirror,
+    terraform_apply_destroy, terraform_init, terraform_output, terraform_plan, terraform_show,
     terraform_validate,
 };
 pub use utils::get_env_var;
