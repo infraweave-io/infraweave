50032818f0a3b673b999e8830b0da5336ef05bd5
InfiniteTabs <18383533+InfiniteTabs@users.noreply.github.com>
2026-05-09 17:34:26 +0200

    chore: remove deprecated webserver-openapi

--- webserver-openapi/src/lib.rs
+++
@@ -1,7 +0,0 @@
-mod auth;
-mod handlers;
-mod server;
-
-pub use auth::{get_internal_token, set_internal_token};
-pub use handlers::ApiDoc;
-pub use server::{run_server, run_server_on_port, run_server_with_listener};
